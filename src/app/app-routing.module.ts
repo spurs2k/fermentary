a51e94e017a86a64c3233693a3981b260d30d9e2
spurs2k <spurlinjames@gmail.com>
2022-10-10 11:55:12 -0500

    added proper routing with functioning header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { UpcomingEventsComponent } from './upcoming-events/upcoming-events.component';
+import { BuildProgressComponent } from './build-progress/build-progress.component';
+import { BlogComponent } from './blog/blog.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'upcoming-events', component: UpcomingEventsComponent},
+  { path: 'build-progress', component: BuildProgressComponent},
+  { path: 'blog', component: BlogComponent},
+  { path: '', component: HomeComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
